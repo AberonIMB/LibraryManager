5b498c3c2e8497911282832f678c5c18c7ef7820
Soldatov Aleksey <Fotaxis@mail.ru>
2024-11-16 18:13:16 +0500

    Переписал тесты на Mockito

--- src/main/java/org/example/model/Book.java
+++ src/main/java/org/example/model/Book.java
@@ -1,6 +1,7 @@
 package org.example.model;
 
 import jakarta.persistence.*;
+import java.util.Objects;
 
 /**
  * Класс книги
@@ -103,5 +104,28 @@ public class Book {
      */
     public String getBookShortInfo() {
         return String.format("[%d] %s - %s (%d)", id, title, author, publicationYear);
+    }
+
+    /**
+     * Сравнивает этот объект с другим объектом на равенство
+     * @return true - если
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return id == book.id
+                && publicationYear == book.publicationYear
+                && Objects.equals(title, book.title)
+                && Objects.equals(author, book.author);
+    }
+
+    /**
+     * Возвращает hashCode объекта
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, author, publicationYear);
     }
 }
